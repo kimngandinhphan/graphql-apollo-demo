cfe0d7235f82e997c004456b4ae716eea8e015d0
nganphan <kimngandinhphan@gmail>
2021-12-28 22:24:47 +0700

    new commit for graphql seminar demo

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema')
+
+const app = express();
+
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}))
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`));
